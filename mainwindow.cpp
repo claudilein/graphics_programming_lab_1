19612856f3d70b59168784d914be514907d717f8
Claudia Pfreundt <claudilein@gmail.com>
2014-04-17 14:14:35 +0200

    added shading menu and about box

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,97 @@
 #include "mainwindow.h"
+#include <QMessageBox>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
+
+    menuBar = new QMenuBar();
+
+
+
+    // == FILE MENU == //
+
+    fileMenu = new QMenu("&File");
+    exitAction = new QAction("E&xit", fileMenu);
+
+    exitAction->setShortcut(QKeySequence::Quit);
+    fileMenu->addAction(exitAction);
+    menuBar->addMenu(fileMenu);
+
+    connect(exitAction, SIGNAL(triggered()), this, SLOT(close()));
+
+
+    // == SHADING MENU == //
+
+    shadingMenu = new QMenu("&Shading");
+    shadingGroup = new QActionGroup(shadingMenu);
+
+    noShadingAction = new QAction("&None (Wireframe)", shadingMenu);
+    noShadingAction->setShortcut(tr("Ctrl+1"));
+    noShadingAction->setCheckable(true);
+    noShadingAction->setIcon(QIcon(":/img/wireframe.png"));
+
+    flatShadingAction = new QAction("Fla&t", shadingMenu);
+    flatShadingAction->setShortcut(tr("Ctrl+2"));
+    flatShadingAction->setCheckable(true);
+    flatShadingAction->setChecked(true);
+    flatShadingAction->setIcon(QIcon(":/img/flat.png"));
+
+    gouraudShadingAction = new QAction("&Gouraud", shadingMenu);
+    gouraudShadingAction->setShortcut(tr("Ctrl+3"));
+    gouraudShadingAction->setCheckable(true);
+    gouraudShadingAction->setIcon(QIcon(":/img/gouraud.png"));
+
+    phongShadingAction = new QAction("&Phong", shadingMenu);
+    phongShadingAction->setShortcut(tr("Ctrl+4"));
+    phongShadingAction->setCheckable(true);
+    phongShadingAction->setIcon(QIcon(":/img/phong.png"));
+
+    shadingGroup->addAction(noShadingAction);
+    shadingGroup->addAction(flatShadingAction);
+    shadingGroup->addAction(gouraudShadingAction);
+    shadingGroup->addAction(phongShadingAction);
+
+    shadingMenu->addAction(noShadingAction);
+    shadingMenu->addAction(flatShadingAction);
+    shadingMenu->addAction(gouraudShadingAction);
+    shadingMenu->addAction(phongShadingAction);
+
+    menuBar->addMenu(shadingMenu);
+
+    // == ABOUT ACTION == //
+
+    aboutAction = new QAction("&About", menuBar);
+    menuBar->addAction(aboutAction);
+
+    connect(aboutAction, SIGNAL(triggered()), this, SLOT(showAboutBox()));
+
+    setMenuBar(menuBar);
 }
+
+
+
+void MainWindow::showAboutBox()
+{
+    QMessageBox msgBox;
+    msgBox.setWindowTitle("About Hello Cube!");
+    msgBox.setText("Written by Claudia Pfreundt!");
+    msgBox.exec();
+}
+
+
 
 MainWindow::~MainWindow()
 {
+    delete menuBar;
+    delete fileMenu;
+    delete shadingMenu;
+    delete shadingGroup;
 
+    delete exitAction;
+    delete noShadingAction;
+    delete flatShadingAction;
+    delete gouraudShadingAction;
+    delete phongShadingAction;
+    delete aboutAction;
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,10 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QMenuBar>
+#include <QMenu>
+#include <QAction>
+//#include <QtGui>
 
 class MainWindow : public QMainWindow
 {
@@ -10,6 +14,29 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = 0);
     ~MainWindow();
+
+public slots:
+    void showAboutBox();
+
+private:
+    QMenuBar *menuBar;
+
+
+    // == FILE MENU == //
+    QMenu *fileMenu;
+    QAction *exitAction;    
+
+    // == SHADING MENU == //
+    QMenu *shadingMenu;
+    QActionGroup *shadingGroup;
+
+    QAction *noShadingAction;
+    QAction *flatShadingAction;
+    QAction *gouraudShadingAction;
+    QAction *phongShadingAction;
+
+    // == ABOUT ACTION == //
+    QAction *aboutAction;
 };
 
 #endif // MAINWINDOW_H
